9df0cdcc5aef585523712507c9c15bf7dd62d2f4
Javier C Weddington <52979080+javiercweddington@users.noreply.github.com>
2025-09-26 20:03:18 -0400

    Add comments about model definitions and usage
    Added comments regarding model definitions and usage.

--- P3-ITSALIVE/model.py
+++ P3-ITSALIVE/model.py
@@ -1,2 +1,6 @@
 import torch
 import numpy as np
+
+#wil have to use the model deifintons from Voltron and BProc despite the tools being pretty much the same
+#Maybe I will have to make generalizable definition for all models.
+

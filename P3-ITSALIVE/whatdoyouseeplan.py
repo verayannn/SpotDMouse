182452b8a8e57bda9fa5a8d3ac23d6229e5816fe
javiercweddington <javiercw@stanford.edu>
2025-10-01 18:14:58 -0400

    MAC: update intro

--- P3-ITSALIVE/whatdoyouseeplan.py
+++ P3-ITSALIVE/whatdoyouseeplan.py
@@ -1,4 +1,4 @@
-#compare all threee repos
+#compare all threee repos -- grok (foghorn), bscope, original paper code
 #write a psudocode for te vanilla IG algorithm
 #write a pseudeocode for SIG algorithm
 #explain the differences between the methods
@@ -9,3 +9,5 @@
 #demonstrate the Shenghua Liu example highlighting the differences between these networks: 0-14,15-FLASH,16-29 revese back to 0
 #demonstrate the contribution differences on a grayscale camera inpuut using ResNet-18, GolDRoger, Charmander to layers 1, 2,3, and penultimate
 #compare trajectory 
+
+

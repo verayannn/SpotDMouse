1fc1c5b7d7c6bbf0d32171aa0d658ab329e941aa
javiercweddington <javiercw@stanford.edu>
2025-10-01 17:58:07 +0000

    DOCKER: sig plan

---
+++ P3-ITSALIVE/whatdoyouseeplan.py
@@ -0,0 +1,11 @@
+#compare all threee repos
+#write a psudocode for te vanilla IG algorithm
+#write a pseudeocode for SIG algorithm
+#explain the differences between the methods
+#pull in the parts from codec that are good and throwaway the ResNet-18 stuff, neuron
+#pull make SIG specific changes to CODEC for all
+#get a contribution map function that thresholds the channels in a layer that controbute the most for both the IG and SIG
+#demonstrate all work on NP WN
+#demonstrate the Shenghua Liu example highlighting the differences between these networks: 0-14,15-FLASH,16-29 revese back to 0
+#demonstrate the contribution differences on a grayscale camera inpuut using ResNet-18, GolDRoger, Charmander to layers 1, 2,3, and penultimate
+ 

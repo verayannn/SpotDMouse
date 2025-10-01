4ce2711fa3ccd3e4a1b92e711258a77ebeb7d1cf
javiercweddington <javiercw@stanford.edu>
2025-10-01 19:33:10 +0000

    DOCKER: update

--- P3-ITSALIVE/whatdoyouseeplan.py
+++ P3-ITSALIVE/whatdoyouseeplan.py
@@ -8,4 +8,4 @@
 #demonstrate all work on NP WN
 #demonstrate the Shenghua Liu example highlighting the differences between these networks: 0-14,15-FLASH,16-29 revese back to 0
 #demonstrate the contribution differences on a grayscale camera inpuut using ResNet-18, GolDRoger, Charmander to layers 1, 2,3, and penultimate
- 
+#compare trajectory 

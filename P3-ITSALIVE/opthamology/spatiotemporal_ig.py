MAC: wrong normalization scale, just use T for the average

P3-ITSALIVE/opthamology/spatiotemporal_ig.py
@@ -15,32 +15,32 @@ def compute_gradients(model, inputs, target_class=None):
         target = output.gather(1,target_class.view(-1,1)).squeeze()
 
     model.zero_grad()
-    target.sum().backward(retain_graph=True)
+    target.sum().backward(retain_graph=True) 
 
     gradients = inputs.grad.detach().clone()
-
+    
     return output.detach(), gradients
 
 def spatiotemporal_integrated_gradients(model, input_tensor, baseline=None, target_class=None, steps_per_segment=50):
 
-    #Initialize X'
     if baseline is None:
         baseline = torch.zeros_like(input_tensor)
 
     assert baseline.shape == input_tensor.shape, "baseline, input shapes mismatch"
 
     device = input_tensor.device
-    batch_size = input_tensor.size(0)
     T = input_tensor.size(1)
-
+    
     sig_attributions = torch.zeros_like(input_tensor)
+    
+    M = steps_per_segment - 1
 
     for t in range(1, T + 1):
-
+        
         betas = torch.linspace(0, 1, steps_per_segment, device=device)
 
         segment_gradients = []
-
+        
         for beta in betas:
 
             X_partial = baseline.clone()
@@ -48,12 +48,15 @@ def spatiotemporal_integrated_gradients(model, input_tensor, baseline=None, targ
             if t > 1:
                 X_partial[:, :t-1] =  input_tensor[:, :t-1]
 
-            X_partial[:, t-1] = baseline[:, t-1] + beta * (input_tensor[:, t-1] - baseline[:, t-1])
+            current_frame_idx = t - 1
+            X_partial[:, current_frame_idx] = baseline[:, current_frame_idx] + beta * \
+                                              (input_tensor[:, current_frame_idx] - baseline[:, current_frame_idx])
+            
             X_partial.requires_grad_(True)
 
             _, grads = compute_gradients(model, X_partial, target_class)
 
-            grad_t = grads[:, t-1].clone()
+            grad_t = grads[:, current_frame_idx].clone()
 
             segment_gradients.append(grad_t)
 
@@ -61,15 +64,14 @@ def spatiotemporal_integrated_gradients(model, input_tensor, baseline=None, targ
 
         IG_t = torch.zeros_like(segment_gradients[0])
 
-        for j in range(steps_per_segment - 1):
+        for j in range(M):
             IG_t += 0.5 * (segment_gradients[j] + segment_gradients[j + 1])
 
-        IG_t = T * IG_t / (steps_per_segment - 1)
+        IG_t = IG_t / M 
 
-        frame_diff = input_tensor[:, t-1] - baseline[:, t-1]
+        frame_diff = input_tensor[:, current_frame_idx] - baseline[:, current_frame_idx]
         SIG_t = frame_diff * IG_t
 
-        sig_attributions[:, t-1] = SIG_t
+        sig_attributions[:, current_frame_idx] = SIG_t
 
-    return sig_attributions 
-
+    return sig_attributions
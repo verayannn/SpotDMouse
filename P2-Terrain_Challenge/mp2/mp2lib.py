6009b1b4aff30bf1ddfd5ab67d0c9c4e6a6f2f6f
Ubuntu <javiercw@stanford.edu>
2025-11-15 06:06:11 +0000

    MP2: looked at all of the MP2 APIs and MD librariies on hardcoded movement communicated through UDP and their ESP32 interface! ROS2 incoming.

--- P2-Terrain_Challenge/mp2/mp2lib.py
+++ P2-Terrain_Challenge/mp2/mp2lib.py
@@ -17,5 +17,19 @@ print(f"Num Positions:{len(positions)}", f"Values: {positions}")
 #Set Positions
 #esp32.servos_set_postion_torque(positon, torque)
 
+#Get IMU Data from sensor
+from MangDang.mini_pupper.ESP32Interface import ESP32Interface
+import time
+
+esp32 = ESP32Interface()
+
+#while True:
+#    print(esp32.imu_get_data())
+#    time.sleep(1 / 20)  # 20 Hz
+
+data = esp32.imu_get_data()
+print("Shape:", len(data) if isinstance(data, (list, tuple)) else "N/A")
+print("Values:", data)
+print("Labels: [ax, ay, az, gx, gy, gz]")
 
 
